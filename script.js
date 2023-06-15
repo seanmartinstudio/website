77d1939192bcc1b1dea7ebf1c496268fdac2c05e
Sean Martin <sean@seanmartinstudio.com>
2023-06-15 13:53:45 -0700

    fetches and renders temp to UI

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
 
-
+//Local Time
 const fetchLocalTime = () => {
     fetch('http://api.timezonedb.com/v2.1/get-time-zone?key=ZMZNE61B4AJ5&format=json&by=position&lat=34.024212&lng=-118.496475')
       .then(response => response.json())
       .then(data => {
         let currentTime = new Date(data.formatted)
         currentTime = currentTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
-        console.log("Current Time Obj: ", currentTime)
         updateCurrentTimeUI(currentTime)
       })
       .catch(error => {
-        console.log('Error fetching local time:', error);
+        console.log('Error fetching local time:', error)
       });
   }
 
-  const updateCurrentTimeUI = (apiTime) => {
-    let element = document.getElementById(`current_time`)
-    element.innerText = apiTime
+    const updateCurrentTimeUI = (apiTime) => {
+        let element = document.getElementById(`current_time`)
+        element.innerText = apiTime
+    }
+
+    fetchLocalTime()
+    setInterval(fetchLocalTime, 60000)
+
+//Local Temp API
+const fetchLocalTemp = () => {
+    fetch('https://api.weatherapi.com/v1/current.json?key=a0fa27db265e4778985202146231506&q=Santa Monica&aqi=no')
+      .then(response => response.json())
+      .then(data => {
+        const currentTemp = data.current.temp_f
+        let roundedFloat = Math.round(parseFloat(currentTemp))
+        let roundedString = roundedFloat.toString()
+        updateCurrentTempUI(roundedString)
+      })
+      .catch(error => {
+        console.log('Error fetching local time:', error)
+      })
   }
 
-  fetchLocalTime()
-setInterval(fetchLocalTime, 60000)
+  const updateCurrentTempUI = (apiTemp) => {
+    let element = document.getElementById(`current_temp`)
+    element.innerText = apiTemp
+}
 
+    fetchLocalTemp()
+    setInterval(fetchLocalTemp, 60000)
