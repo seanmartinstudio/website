ee313328ebb57b27537d4f0e019214fdc8bfd11a
Sean Martin <sean@seanmartinstudio.com>
2023-06-20 16:10:32 -0700

    removes wrong CORS from script.js

--- script.js
+++ script.js
@@ -2,21 +2,6 @@
 console.log("%c \n© Sean Martin 2023\n", "color: grey")
 
 
-//CORS
-import express from 'express';
-var cors = require('cors')
-var app = express()
-
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
 
 //Local Time
 const fetchLocalTime = () => {
